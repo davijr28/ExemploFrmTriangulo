aa1065756e24ba58777e5d363c2e37a05df8d837
davijr28 <davijr28@gmail.com>
2024-10-23 10:53:12 -0300

    init: início do projeto

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,11 @@
+package principal;
+
+import view.FrmTriangulo;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        FrmTriangulo janela = new FrmTriangulo();
+        janela.setVisible(true);
+    }
+}
